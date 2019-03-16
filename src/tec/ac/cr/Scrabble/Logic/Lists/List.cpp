9275e2d76a813e345872174dc192b76768870a62
chus <sandoval14yamir@hotmail.com>
2019-03-16 17:04:19 -0600

    First Commit

---
+++ src/tec/ac/cr/Scrabble/Logic/Lists/List.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chus on 16/03/19.
+//
+
+#include "List.h"
---
+++ src/tec/ac/cr/Scrabble/Logic/Lists/List.h
@@ -0,0 +1,14 @@
+//
+// Created by chus on 16/03/19.
+//
+
+#ifndef SCRABBLE_LIST_H
+#define SCRABBLE_LIST_H
+
+
+class List {
+
+};
+
+
+#endif //SCRABBLE_LIST_H
