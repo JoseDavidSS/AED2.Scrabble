b594c39238c7deeed7038398112c22bc4c0d5ebf
chus <sandoval14yamir@hotmail.com>
2019-03-17 14:55:09 -0600

    Inicio la creacion del algoritmo search para el matching de palabras

---
+++ src/tec/ac/cr/Scrabble/Logic/Lists/ListSearch.cpp
@@ -0,0 +1,10 @@
+//
+// Created by chus on 17/03/19.
+//
+
+#include "ListSearch.h"
+using namespace std;
+
+void ListSearch::search(string words){
+
+}
---
+++ src/tec/ac/cr/Scrabble/Logic/Lists/ListSearch.h
@@ -0,0 +1,21 @@
+//
+// Created by chus on 17/03/19.
+//
+
+#ifndef SCRABBLE_LISTSEARCH_H
+#define SCRABBLE_LISTSEARCH_H
+#include <string>
+using namespace std;
+
+
+class ListSearch {
+
+public:
+    ListSearch() = default;
+
+    void search(string Words );
+
+};
+
+
+#endif //SCRABBLE_LISTSEARCH_H
