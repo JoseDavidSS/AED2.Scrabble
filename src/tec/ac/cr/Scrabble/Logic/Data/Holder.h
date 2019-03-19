f4fcec7cbc734818a4a8b2396de9f9e91bb04f83
Kevin Cordero <skryfall@gmail.com>
2019-03-19 09:40:56 -0600

    Anadido un atributo al Holder

--- src/tec/ac/cr/Scrabble/Logic/Data/Holder.h
+++ src/tec/ac/cr/Scrabble/Logic/Data/Holder.h
@@ -22,10 +22,11 @@ private:
     Holder& operator = (Holder const&) = default;
 
     static Holder* holder;
-    bool turn;
-    int points;
-    string playerName;
-    int codeToEnter;
+    bool turn; //Este indica si es el turno del jugador o no
+    bool validatedPlay; // Este indica si la jugada es validada por el server
+    int points; //Puntos del jugador
+    string playerName; //Nombre del jugador
+    int codeToEnter; //Codigo para ingresar a una partida o para crear una
     //Lista con las letras actuales del jugador
     //Matriz (lista de listas) del juego
 
