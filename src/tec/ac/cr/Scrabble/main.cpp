6fcbf287d67fb7b3b9ca11564ec33ad8ae33b7bf
Jose <jose.david.s@gmail.com>
2019-03-24 12:33:25 -0600

    Se trabaja el serializador y deserializador.

--- src/tec/ac/cr/Scrabble/main.cpp
+++ src/tec/ac/cr/Scrabble/main.cpp
@@ -1,6 +1,7 @@
 #include "Server/Client.h"
 #include "Logic/Lists/List.h"
 #include "Logic/Lists/Matrix.h"
+#include "Logic/Player.h"
 
 #include <iostream>
 
@@ -8,6 +9,13 @@ using namespace std;
 
 int main()
 {
+    Player* p1 = new Player("Jose");
+    p1->setMessage("LEON",1,1,1,4);
+    p1->toString();
+    string json = p1->serialize();
+    cout<<json<<endl;
+    Player* p2 = p1->deserealize(json.c_str());
+    p2->toString();
 
     // Create test lists
     List l;
